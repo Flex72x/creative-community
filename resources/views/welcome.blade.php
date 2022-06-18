47aab6ff2b18413f2f1b3398a2c6e010d290aa5c
Flex72x <multimak01@gmail.com>
2022-06-18 15:42:45 +0500

    123

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/styles.css">
+
+    <script src="/js/jquery-3.6.0.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <title>Document</title>
+
+    <script>
+        
+
+        
+    </script>
+</head>
+<body class="position-relative">
+    <nav id="menu" class="navbar navbar-expand-lg navbar-dark bg-dark d-block d-md-none" style="position: sticky; top: 0; z-index: 999;">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="https://media.discordapp.net/attachments/987584287958269962/987638589141692426/logo.png" width="100"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active mx-4" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mx-4" href="#">Link</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+    </nav>
+    
+
+    <div class="container-fluid block1 d-flex justify-content-center alihn-items-center flex-column position-relative">
+        <p style="position: absolute; top: 20px;"><img src="https://media.discordapp.net/attachments/987584287958269962/987638589141692426/logo.png" width="100"></p>
+        <h1 class="text-white text-center fw-gbold display-1">Студия дизайна и веб-разработки</h1>
+        <p class="text-white fs-2 mt-2 text-center">Мы создаем качественные веб-сайты <a href="#about" class="btn btn-lg btn-orange text-white ms-3">Подробнее</a></p>
+        <p class="mt-4"></p>
+    </div>
+
+    <div class="container-fluid" style="background: #ebebed;">
+        <div class="container py-4">
+            <div class="row justify-content-center">
+                <div class="col-xxl-3">
+                    <p class="text-center fs-1"><img src="https://media.discordapp.net/attachments/987584287958269962/987652566601052180/proof-reading.png" width="100"></p>
+                    <h3 class="text-center fw-gbold">Изучаем</h3>
+                    <p class="text-center">Спрашиваем, слушаем, собираем информацию, резюмируем.</p>
+                </div>
+
+                <div class="col-xxl-3">
+                    <p class="text-center fs-1"><img src="https://media.discordapp.net/attachments/987584287958269962/987652566848520252/programming.png" width="100"></p>
+                    <h3 class="text-center fw-gbold">Вносим пожелания</h3>
+                    <p class="text-center">Объединяя ваши задачи с нашими идеями, улучшаем проект.</p>
+                </div>
+
+                <div class="col-xxl-3">
+                    <p class="text-center fs-1"><img src="https://media.discordapp.net/attachments/987584287958269962/987652567062437948/monitor.png" width="100"></p>
+                    <h3 class="text-center fw-gbold">Строим</h3>
+                    <p class="text-center">Проект, отвечающий современным трендам.</p>
+                </div>
+
+                <div class="col-xxl-3">
+                    <p class="text-center fs-1"><img src="https://media.discordapp.net/attachments/987584287958269962/987652567267954768/startup.png" width="100"></p>
+                    <h3 class="text-center fw-gbold">Запускаем</h3>
+                    <p class="text-center">После внимательного изучения и тестирования, публикуем проект.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid" id="about">
+        <div class="container py-4">
+            <h2 class="text-center fw-gbold">Услуги</h2>
+            <p class="text-center">Делаем проекты, которыми бы хотели пользоваться сами</p>
+            <hr>
+            <div class="row mt-3">
+                <div class="col-xxl-3 text-center">
+                    <div class="mb-3">
+                        <img src="https://cdn.icon-icons.com/icons2/1092/PNG/512/graphicdesign2_78420.png" class="w-50">
+                    </div>
+                    Веб-дизайн сайтов
+                </div>
+
+                <div class="col-xxl-3 text-center">
+                    <div class="mb-3">
+                        <img src="https://cdn.icon-icons.com/icons2/1092/PNG/512/appdevelopment_78410.png" class="w-50">
+                    </div>
+                    Вертска сайтов
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid py-4">
+        <div class="container">
+            <h3 class="text-center fw-gbold">Как с нами связаться</h3>
+            <p class="text-center">Оставьте свои контакты и мы свяжемся с вами в ближайшее время</p>
+            <form action="" class="col-xxl-6 ms-auto me-auto">
+                <div class="row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Имя">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Телефон">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="E-mail">
+                    </div>
+                </div>
+
+                <div class="row mt-2">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Комментарий">
+                    </div>
+                </div>
+
+                <div class="row mt-2 text-center">
+                    <button class="btn btn-orange">Связаться</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="container-fluid bg-dark py-4">
+        <div class="container">
+            <h2 class="fw-gbold text-center text-white mb-4">Контакты</h2>
+            <div class="row text-white mt-4">
+                <div class="col-xxl-4 text-center">+7 9-600-429-700</div>
+                <div class="col-xxl-4 text-center">mail@mail.ri</div>
+                <div class="col-xxl-4 text-center">afaffa</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
